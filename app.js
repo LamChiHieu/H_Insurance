49711e9088e938e007a3b5ee1331b73fb8cea6df
Lam Chi Hieu <lamchihieu.vnn@gmail.com>
2021-05-07 16:02:51 +0700

    cm

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
 app.listen(3000);
+
+//middleware: parse application/x-www-form-urlencoded: 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//add view engine
+app.set("view engine", "ejs");
+
 
 app.get('/', function(req,res){
     res.send('Home123')
